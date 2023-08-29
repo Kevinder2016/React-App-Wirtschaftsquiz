b41f125376b451dcedec07e83f15f5591cb541c6
Kevinder2016 <69578309+Kevinder2016@users.noreply.github.com>
2023-08-29 17:38:59 +0200

    Add files via upload

---
+++ React-App-Wirtschaftsquiz/README.md
@@ -0,0 +1,11 @@
+# Wirtschaftsquiz
+ 
+ Melanie, Oltian, Luka und Kevin
+
+## Was ist das?
+
+Für unsere Interdisziplinäre Projektarbeit (IDPA) haben wir uns dazu entschieden, eine Website zu programmieren, auf welcher man sein Wissen über die Betriebswirtschaft testen kann. Dabei haben wir uns an unserem Lernmittel "Betriebswirtschaft - Theorie & Aufgaben" vom KLV Verlag orientiert.
+
+## Wer sind wir?
+
+Wir sind vier Schüler der Informatikmittelschule Baden und diese Arbeit ist, wie bereits erwähnt, unsere IDPA.
---
+++ React-App-Wirtschaftsquiz/src/index.js
@@ -0,0 +1,83 @@
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import React from "react";
+
+//Alli Komponente wo brucht werde werde importiert
+import Login from "./Login.js";
+import Quiz from "./Quiz.js";
+import Navbar from "./Navbar.js";
+import Home from "./Home.js";
+import Kapitelwahl from "./Kapitelwahl.js"
+
+//Root Element und Navbar Element werde im index.html File gsuecht und usgwählt
+const root = ReactDOM.createRoot(document.getElementById("root"));
+const navbar = ReactDOM.createRoot(document.getElementById("navbar"));
+
+//die eigete Userdate werde lokal gspeicheret demit mer bim reload ned automatisch usgloggt wird
+var loggedInUserEmail = localStorage.getItem("loggedInUserEmail");
+var loggedInUserPass = localStorage.getItem("loggedInUserPass");
+var loggedInUserPunkte = localStorage.getItem("loggedInUserPunkte");
+
+
+//Zeigt s Login im root Element ah
+export const renderLogin = (isRegister) => {
+  localStorage.setItem("loggedInUserEmail", null)
+  localStorage.setItem("loggedInUserPass", null)
+  localStorage.setItem("loggedInUserPunkte", null)
+  root.render(<Login register={isRegister} />);
+};
+
+//Loggt us idem dass es de Navbar en leere User mitgit und d Homesite azeigt
+export const renderLogout = () => {
+  localStorage.setItem("loggedInUserEmail", null)
+  localStorage.setItem("loggedInUserPass", null)
+  localStorage.setItem("loggedInUserPunkte", null)
+  //Zeigt d Navigation im navbar-Element (neu) ah
+  navbar.render(<Navbar user={null} />);
+  //Zeigt d Startsite im root-Element ah
+  root.render(<Home />);
+};
+
+//Zeigt s Quiz im root-Element ah
+//nimmt Kapitel und de aktuell igloggti Nutzer entgege und gits em Quiz witer
+export const renderQuiz = (kapitel, user) => {
+  localStorage.setItem("loggedInUserEmail", user.email)
+  localStorage.setItem("loggedInUserPass", user.passwort)
+  localStorage.setItem("loggedInUserPunkte", user.punkte)  
+  root.render(<Quiz kapitel={kapitel} user={user} />);
+};
+
+//Zeigt d Kapitelwahl im root-Element ah
+//nimmt de aktuell igloggti Nutzer entgege und gits de Kapitelsite witer zums denn em Quiz witerzgä
+export const renderKapitelwahl = (user) => {
+  localStorage.setItem("loggedInUserEmail", user.email)
+  localStorage.setItem("loggedInUserPass", user.passwort)
+  localStorage.setItem("loggedInUserPunkte", user.punkte)
+  root.render(<Kapitelwahl user={user} />)
+};
+
+//Zeigt d Navbar im navbar-Element ah
+//nimmt de aktuell igloggti Nutzer entgege und gits de Navbar witer
+export const renderNavbar = (n) => {
+  navbar.render(<Navbar user={n} />);
+};
+
+//Zeigt d Startsite im root-Element ah
+export const renderHome = () => {
+  localStorage.setItem("loggedInUserEmail", null)
+  localStorage.setItem("loggedInUserPass", null)
+  localStorage.setItem("loggedInUserPunkte", null)
+  root.render(<Home />);
+};
+
+//wenn die date lokal gfunde werde könne und ned "null" sind (als string) wird s d kapitelwahl azeigt und mer isch igloggt
+if(loggedInUserEmail !== null && loggedInUserPass !== null && loggedInUserPunkte !== null && loggedInUserEmail !== "null" && loggedInUserPass !== "null" && loggedInUserPunkte !== "null"){
+  var user = {email: loggedInUserEmail, passwort: loggedInUserPass, punkte: loggedInUserPunkte};
+  renderNavbar(user);
+  renderKapitelwahl(user);
+} 
+//süsch het me sich zletzt usgloggt oder no gar ke account
+else {
+  renderNavbar(null);
+  renderHome();
+}
